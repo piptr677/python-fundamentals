3a0cf908ff9bbb17c6872427a15e7d04f07eae7d
Piotr Paluch <justjaylol67@gmail.com>
2026-06-29 14:32:20 +0200

    finish variables and types exercises

---
+++ 02_variables_and_types/numbers.py
@@ -0,0 +1,9 @@
+myint = 7
+print(myint)
+
+
+myfloat = 7.0
+print(myfloat)
+
+myfloat2 = float(7)
+print(myfloat2)
---
+++ 02_variables_and_types/strings.py
@@ -0,0 +1,22 @@
+mystring = 'hello'
+#print(mystring)
+
+mystring2 = "hello"
+#print(mystring2)
+
+mystring3 = "Don't worry about apostrophes"
+#print(mystring3)
+
+one = 1
+two = 2
+three = one + two
+#print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+#print(helloworld)
+
+
+a, b = 3, 4
+print(a, b)
