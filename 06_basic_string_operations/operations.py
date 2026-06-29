096a6b264614a98f9dff36f04c7863a410751acc
Piotr Paluch <justjaylol67@gmail.com>
2026-06-29 16:10:37 +0200

    add new exercises

---
+++ 06_basic_string_operations/operations.py
@@ -0,0 +1,16 @@
+astring = "Hello world!"
+#print("single quotes are ' '")
+
+#print(len(astring))
+#print(astring.index("o"))
+#print(astring.count("l"))
+#print(astring[3:7])
+
+#print(astring.upper())
+#print(astring.lower())
+
+#print(astring.startswith("Hello"))
+#print(astring.endswith("fdsfrfdsfds"))
+
+afewwords = astring.split(" ")
+print(afewwords)
---
+++ 07_conditions/conditions.py
@@ -0,0 +1,27 @@
+#x = 2
+#print (x == 2)
+#print (x == 3)
+#print (x < 3)
+
+#name = "John"
+#age = 23
+#if name == "John" and age == 23:
+#    print("Your name is John, and you are also 23 years old.")
+
+#if name == "John" or name == "Rick":
+#    print("Your name is either Rick or John")
+
+#if name in ["John", "Rick"]:
+#    print("Your name is either John or Rick.")
+
+#if x == 2:
+#    print ("x equals two")
+#else:
+#    print("x does not equal two")
+
+x = [1,2,3]
+y = [1,2,3]
+print(x == y) #True
+print(x is y) #False
+
+
