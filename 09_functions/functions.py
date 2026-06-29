c7ec3f86eda7788453dc447e663712f0550cbb6d
Piotr Paluch <justjaylol67@gmail.com>
2026-06-29 21:23:07 +0200

    add function exercises

---
+++ 09_functions/functions.py
@@ -0,0 +1,9 @@
+def my_function(username, greeting):
+    print("Hello, %s , My Function!, I wish you %s" % (username, greeting))
+
+my_function("lol", "hi")
+
+def sum_two_numbers(a, b):
+    return a + b
+
+print(sum_two_numbers(2, 4))
